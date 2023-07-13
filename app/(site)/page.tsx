c43d320ced1a09e921f24ce2481e177bd3ce9c2d
Vishmi98 <125298089+Vishmi98@users.noreply.github.com>
2023-07-13 22:06:01 +0530

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           height="48"
           width="48"
           className="mx-auto w-auto"
-          src="https://beemden.nl/wp-content/uploads/2021/08/buddy-tekst.png"
+          src="/images/buddy.png"
         />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in your account
