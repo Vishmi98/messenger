c74e22e5b7319290e6beaee95d2a34aab6e373cb
Vishmi98 <125298089+Vishmi98@users.noreply.github.com>
2023-07-13 22:08:35 +0530

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="Logo"
-          height="48"
-          width="48"
+          height="40"
+          width="60"
           className="mx-auto w-auto"
           src="/images/buddy.png"
         />
